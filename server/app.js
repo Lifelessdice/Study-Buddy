5340248c296f5c173b52be1303f814329a527a93
Mirza Raian Ahmad <raian@student.chalmers.se>
2025-11-23 09:07:15 +0100

    mounted AI quiz

--- server/app.js
+++ server/app.js
@@ -20,6 +20,8 @@ var quizParticipationRoutes = require('./routes/quizParticipations');
 var courseAttendancesRoutes = require('./routes/courseAttendances');
 var teachingAssignmentRoutes = require('./routes/teachingAssignments');
 var noteSummariesRoutes = require('./routes/noteSummaries');
+var noteAIquizRoutes = require('./routes/noteAIquiz');
+
 
 
 // ---------------------------------------------
@@ -67,6 +69,8 @@ app.use(API_PREFIX + "/quizparticipations", quizParticipationRoutes);
 app.use(API_PREFIX + "/courses", courseAttendancesRoutes);
 app.use(API_PREFIX + "/courses", teachingAssignmentRoutes);
 app.use(API_PREFIX + "/notes", noteSummariesRoutes);
+app.use(API_PREFIX + "/notes", noteAIquizRoutes);
+
 
 
 // When these routers exist, mount them like this:
