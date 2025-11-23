e66ab45a2dfced2565a7415dbda1a03d2f093302
Amil <amil@student.chalmers.se>
2025-11-24 00:25:13 +0100

    Fix Minor User Bug

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('../models/user');
+const User = require('../models/users');
 
 // Create User
 exports.createUser = async (req, res, next) => {
